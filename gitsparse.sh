522dcd7dd45cbd1ec0e63c736dbc6d55316fcc3b
Liumeo <liumeo@pku.edu.cn>
2019-04-26 14:27:42 +0800

    destination path should be empty

--- gitsparse.sh
+++ gitsparse.sh
@@ -2,9 +2,13 @@ function gitsparse {
 	if [ -z $1 ];then
 		echo 'Usage: gitsparse git_url file_1 [... file_n]'
 		return
-       	fi
+    fi
 	local dir_name=${1##*/}
-	mkdir $dir_name
+	if [[ -d $dir_name && `ls -A $dir_name | wc -w` -gt 0 ]];then
+		echo "fatal: destination path '${dir_name}' already exists and is not an empty directory."
+		return
+    fi
+	mkdir -p $dir_name
 	pushd $dir_name >/dev/null
 	git init
 	git config core.sparsecheckout true
